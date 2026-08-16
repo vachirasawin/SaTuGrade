a8122c98a597deac6bdd3cef05f42401efce2dbc
vachirasawin <vachirasawin.mah@gmail.com>
2026-08-16 23:20:38 +0700

    Vercel Speed Insights

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { AuthProvider } from "./Providers";
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata = {
   title: "SaTuGrade",
@@ -18,6 +19,7 @@ export default function RootLayout({ children }) {
       <body className = "antialiased">
         <AuthProvider>{children}</AuthProvider>
         <Analytics/>
+        <SpeedInsights/>
       </body>
     </html>
   );
