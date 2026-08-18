aaaebdd0abe2890f42778f14732cfb58cf37cc8f
vachirasawin <vachirasawin.mah@gmail.com>
2026-08-18 17:08:52 +0700

    Subjects Data

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,6 +2,7 @@ import React from "react"
 
 import Navbar from "./components/navbar/Navbar"
 import Banner from "./components/Banner"
+import SubjectsSection from "./components/section/SubjectsSection"
 import ManualsSection from "./components/section/ManualsSection"
 import FAQsSection from "./components/section/FAQsSection"
 import FrontendSection from "./components/section/FrontendSection"
@@ -19,6 +20,7 @@ async function page() {
             <Navbar/>
             <Banner title = "การพัฒนาเว็บแอปพลิเคชันวิเคราะห์และพยากรณ์แนวโน้มผลการเรียนวิชาหลักเฉพาะสายการเรียนด้วยปัญญาประดิษฐ์เพื่อการวางแผนการเรียน" image = "/classroom.png"/>
             <Project/>
+            <SubjectsSection/>
             <ManualsSection/>
             <FAQsSection/>
             <Banner title = "ระบบปัญญาประดิษฐ์สำหรับพยากรณ์แนวโน้มผลการเรียนวิชาหลักเฉพาะสายการเรียน" image = "/ai.jpg"/>
